df9a5485ea8cef30f3d22245a4c9fae58a3b23b4
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-03-06 16:40:59 +0100

    la suite

--- TP1_Spark/src/main/java/org/example/Application2.java
+++ TP1_Spark/src/main/java/org/example/Application2.java
@@ -1,10 +1,13 @@
 package org.example;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Application2 {
     public static void main(String[] args) {
@@ -13,7 +16,12 @@ public class Application2 {
         JavaSparkContext sc=new JavaSparkContext(sparkConf);
         JavaRDD<String> lines=sc.textFile("names.txt");
         JavaRDD<String> words=lines.flatMap((line)-> Arrays.asList(line.split(" ")).iterator());
-        words.collect();
+        JavaPairRDD<String,Integer> wordPairs=words.mapToPair((word)->new Tuple2<>(word,1));
+        JavaPairRDD<String,Integer> wordPairsCount=wordPairs.reduceByKey((a,b)->a+b);
+        List<Tuple2<String,Integer>> wordCount = wordPairsCount.collect();
+        for (Tuple2<String,Integer> word:wordCount){
+            System.out.println(word._1()+" "+word._2());
+        }
     }
 
 }
